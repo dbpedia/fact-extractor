d08b84c2a996f89535db60ef81f0478a94b43044
Marco Fossati <fossati@spaziodati.eu>
2014-11-14 17:51:22 +0100

    run treetagger on a single article instead of the big file

--- extract_verbs.sh
+++ extract_verbs.sh
@@ -4,12 +4,15 @@ set -e
 
 cd ..
 # Download latest Italian Wikipedia dump
-#wget http://download.wikimedia.org/itwiki/latest/itwiki-latest-pages-articles.xml.bz2
+wget http://download.wikimedia.org/itwiki/latest/itwiki-latest-pages-articles.xml.bz2
 # Extract text
-#bzcat itwiki-latest-pages-articles.xml.bz2 | scripts/WikiExtractor.py
+bzcat itwiki-latest-pages-articles.xml.bz2 | scripts/WikiExtractor.py
 # Build a single big file
 find extracted -type f -exec cat {} \; > all-extracted.txt
 # Extract verbs with TreeTagger
-cat all-extracted.txt | treetagger/cmd/tree-tagger-italian | grep VER | sort -u > verbi.txt
+# N.B. treetagger segfaults with the single big file, run it over each article instead
+#cat all-extracted.txt | treetagger/cmd/tree-tagger-italian | grep VER | sort -u > verbi.txt
+find extracted -type f -exec bash -c "cat '{}' | treetagger/cmd/tree-tagger-italian | grep VER >> verbi.txt" \;
+sort -u verbi.txt > unique-sorted-verbs.txt
 # Extract vocabulary
 python scripts/bag_of_words.py all-extracted.txt
