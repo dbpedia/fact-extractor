13e9f028ed171252631e5979f1fbf43da09c30fc
Marco Fossati <fossati@spaziodati.eu>
2014-11-21 11:14:10 +0100

    top 50 verb lemmas without auxiliayr/modal verbs + full matching with evalita 2008 data

--- post_process.sh
+++ post_process.sh
@@ -8,11 +8,14 @@ cut -f 3 true-positives.txt | sort -u > lemmas.txt
 python make_lemma_freq.py
 # 100 most frequent
 grep -P '\d{3},' frequencies.json | cut -d '"' -f 2 > over-100-occurrencies
-# N.B. modal + auxiliar verbs commented by hand
-# Intersect with Saccarosio's frame annotation dataset and get matching sentence IDs
-# FIXME no lemmas in annotation, change matching strategy
-grep -wf ../verbs/over-100-occurrencies FBK/ita_gold.xml | grep -oP 'id="[^"]+"' | sed 's/id="//' | sed 's/"//' > FBK/matching-ids
+# Top 50 without auxiliar/modal verbs (very frequent of course)
+grep -vP 'avere|essere|fare|potere|volere|dovere' frequencies.json | sed -n '2,52'p > top-50
+# Top 50 lemmas with grep magic
+grep -Po '"\K[^"]+(?=")' top-50 | sort -u > top-50-sorted-lemmas
 
+###
+# Intersect with Saccarosio's frame annotation dataset and get what matches
+###
 # SRL annotation
 cat ILC/ILC_Sample_*.sem FBK/FBK_Sample.sem >> annotated
 # LUs evoking frames
@@ -31,6 +34,10 @@ grep -P '\tV\t' pos-tagged | cut -f 2,3 | grep -v '<unknown>' > lexical-units/ve
 grep -wf lexical-units/unique-sorted lexical-units/verb-lemmas > intersection
 # Intersect with our list of verbs
 # N.B. Seems to yield the same set
-grep -f ../verbs/unique-lemmas.txt intersection | cut -f 2 | sort -u > matching-lemmas
-# Final set of frames in the Italian Wikipedia that appear in the annotated dataset
-cut -f 2 frames | sort -u > unique-sorted-frames
+grep -wf ../verbs/unique-sorted-lemmas intersection > ../matching/all
+# Verb lexicalizations
+cut -f 1 ../matching/all > ../matching/verbs
+# Verbs + frames
+grep -wf ../matching/verbs with-frames > ../matching/verbs-frames
+# Final set of frames (with frequencies) in the Italian Wikipedia that appear in the annotated dataset
+python ../../scripts/make_frames_freq.py verbs-frames
