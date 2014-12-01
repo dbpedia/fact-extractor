687a2d16d9d51ac9cc8b1b9eb518165e1cdb53fd
Marco Fossati <fossati@spaziodati.eu>
2014-12-01 11:21:29 +0100

    input text as cli arg

--- split_sentence.py
+++ split_sentence.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 import codecs
+import sys
+
 from nltk.tokenize.punkt import PunktTrainer, PunktSentenceTokenizer
 training = ''.join(codecs.open('IT-TrainingCorpus.txt', 'rb', 'utf-8').readlines())
 trainer = PunktTrainer()
 trainer.train(training, verbose=True)
 tokenizer = PunktSentenceTokenizer(trainer.get_params(), verbose=True)
-text = ''.join(codecs.open('../training/itwiki/gold', 'rb', 'utf-8').readlines())
+text = ''.join(codecs.open(sys.argv[1], 'rb', 'utf-8').readlines())
 sentences = tokenizer.tokenize(text)
 clean = [s for s in sentences if s.find('<strong>') != -1]
 codecs.open('clean-gold', 'wb', 'utf-8').writelines([s + '\n' for s in clean])
