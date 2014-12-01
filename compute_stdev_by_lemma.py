712037012b9c21e09fee5d80650f97b0f1d6af02
Marco Fossati <fossati@spaziodati.eu>
2014-12-01 11:22:11 +0100

    group verb tokens by lemma and sum stdev scores

---
+++ compute_stdev_by_lemma.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+import codecs
+import json
+from collections import defaultdict, OrderedDict
+
+i = codecs.open('top-50-token2lemma.sorted', 'rb', 'utf-8')
+j = defaultdict(list)
+lines = [l.strip() for l in i.readlines()]
+for l in lines:
+    items = l.split('\t')
+    j[items[1]].append(items[0])
+    
+g = json.load(codecs.open('/home/fox/srl/ranking/top-50/tokens/stdevs.json', 'rb', 'utf-8'), encoding='utf-8')
+jg = {}
+for lemma, tokens in j.iteritems():
+    jg[lemma] = 0.0
+    for toke in tokens:
+        jg[lemma] += g.get(toke, 0.0)
+        
+d = OrderedDict(sorted(jg.items(), key=lambda x: x[1], reverse=True))
+json.dump(d, open('../ranking/top-50/stdevs-by-lemma.json', 'wb'), ensure_ascii=False, indent=2)
