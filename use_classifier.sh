c03c5960557ab8c747eb801042084c8adeff3648
Marco Fossati <fossati@spaziodati.eu>
2014-12-03 18:27:29 +0100

    prettier training + classification script

--- use_classifier.sh
+++ use_classifier.sh
@@ -1,16 +1,36 @@
+#!/bin/bash
+
+set -e
+
+usage="Usage: $(basename "$0") <> <> <>"
+if [[ $# -ne 3 ]]; then
+    echo $usage
+    exit 1;
+fi
+
+TREETAGGER_HOME=$1
+TREETAGGER_MODEL=$1/lib/italian-utf8.par
+ANNOTATION_FILE=$2
+JAVA_OPTS="-Dfile.encoding=UTF-8 -cp target/fatJar.jar"
+MAIN_PACKAGE="org.fbk.cit.hlt.dirha"
+GAZETTEER_FILE="resources/it/gazetteer.tsv"
+
 # Tokenize + POS tag a file with raw sentences into a tsv ready for annotation
-java -Dfile.encoding=UTF-8 -Dtreetagger.home=/home/fox/srl/treetagger -Dtreetagger.model=/home/fox/srl/treetagger/lib/italian-utf8.par -cp fatJar.jar org.fbk.cit.hlt.dirha.SentencesToSpreadSheet ../../training/itwiki/gold test.out 0 10
+#java -Dfile.encoding=UTF-8 -Dtreetagger.home=$TREETAGGER_HOME -Dtreetagger.model=$TREETAGGER_MODEL -cp target/fatJar.jar $MAIN_PACKAGE.SentencesToSpreadSheet ../../training/itwiki/gold test.out 0 10
+echo "Learning roles ..."
 # Translate a tsv annotation file into IOB2 format
-java -Dfile.encoding=UTF-8 -cp target/fatJar.jar org.fbk.cit.hlt.dirha.SpreadSheetToRoleTrainingSet -t data/whole-train/20140616/whole-train.tab
+java $JAVA_OPTS ${MAIN_PACKAGE}.SpreadSheetToRoleTrainingSet -t $ANNOTATION_FILE
 # Produce role training data for libsvm
-java -Dfile.encoding=UTF-8 -cp target/fatJar.jar org.fbk.cit.hlt.dirha.RoleTrainingSetToLibsvm -t data/whole-train/20140616/whole-train.tab.iob2 -g resources/gazetteer.tsv
-# Translate a tsv annotation file into text categorization format
-java -Dfile.encoding=UTF-8 -cp target/fatJar.jar org.fbk.cit.hlt.dirha.SpreadSheetToFrameTrainingSet -t data/whole-train/whole-train.tab
-# Produce frame training data for libsvm
-java -Dfile.encoding=UTF-8 -cp target/fatJar.jar org.fbk.cit.hlt.dirha.FrameTrainingSetToLibsvm -t data/whole-train/whole-train.tab.frame -g resources/gazetteer.tsv
+java $JAVA_OPTS ${MAIN_PACKAGE}.RoleTrainingSetToLibsvm -t ${ANNOTATION_FILE}.iob2 -g $GAZETTEER_FILE
 # Train role classifier
-svm-train -t 0 -m 10000 data/whole-train/20140616/whole-train.tab.svm data/whole-train/20140616/whole-train.tab.model
-#Train frame classifier
-svm-train -t 0 -m 10000 data/whole-train/20140821/whole-train.tab.frame.svm data/whole-train/20140821/whole-train.tab.frame.model
+svm-train -t 0 -m 10000 ${ANNOTATION_FILE}.svm ${ANNOTATION_FILE}.model
+echo "Learning frames ..."
+# Translate a tsv annotation file into text categorization format
+java $JAVA_OPTS ${MAIN_PACKAGE}.SpreadSheetToFrameTrainingSet -t $ANNOTATION_FILE 
+# Produce frame training data for libsvm
+java $JAVA_OPTS ${MAIN_PACKAGE}.FrameTrainingSetToLibsvm -t ${ANNOTATION_FILE}.frame -g $GAZETTEER_FILE
+# Train frame classifier
+svm-train -t 0 -m 10000 ${ANNOTATION_FILE}.frame.svm ${ANNOTATION_FILE}.frame.model
+echo "Done! Launching classifier ..."
 # Run classifier in Italian, interactive mode
-java -Dfile.encoding=UTF-8 -Dtreetagger.home=/home/fox/srl/treetagger -cp target/fatJar.jar org.fbk.cit.hlt.dirha.Annotator -g resources/it/gazetteer.tsv -m data/whole-train/20140616/whole-train.tab -i -l it
+java $JAVA_OPTS $TREETAGGER_HOME ${MAIN_PACKAGE}.Annotator -g $GAZETTEER_FILE -m ${ANNOTATION_FILE} -l it -i
