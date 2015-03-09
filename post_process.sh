47099c35be28879e7597f32c023009cb6759d22f
Marco Fossati <fossati@spaziodati.eu>
2015-03-09 12:57:23 +0100

    improved verb ranking

--- post_process.sh
+++ post_process.sh
@@ -6,9 +6,16 @@ shuf true-positives.txt | head -1000 > 1ktest
 # Cut out entries identified as verbs with unknown lemma (probably errors)
 grep -v unknown all.txt > true-positives.txt
 # Lemma list
-cut -f 3 true-positives.txt | sort -u > lemmas.txt
+cut -f 3 true-positives.txt > lemmas
+cut -f 3 true-positives.txt | sort -u > unique-lemmas
+# Token to lemma map
+cut -f 1,3 true-positives.txt | sort -u > token2lemma
+
+######################
+# Verb ranking
+######################
 # Frequency dict
-python make_lemma_freq.py
+python make_lemma_freq.py lemmas
 # 100 most frequent
 grep -P '\d{3},' frequencies.json | cut -d '"' -f 2 > over-100-occurrencies
 # Top 50 without auxiliar/modal verbs (very frequent of course)
@@ -17,6 +24,12 @@ grep -vP 'avere|essere|fare|potere|volere|dovere' frequencies.json | sed -n '2,5
 grep -Po '"\K[^"]+(?=")' top-50 | sort -u > top-50-sorted-lemmas
 # Get tokens from the top 50 lemmas
 grep -wf top-50-sorted-lemmas true-positives | cut -f 1 | perl -ne 'print lc' | sort -u > top-50-sorted-tokens
+# Top 50 token to lemma map
+grep -wf top-50-sorted-lemmas token2lemma > top-50-token2lemma
+# TF/IDF-based token ranking
+python tf_idfize.py ../players-corpus top-50-sorted-tokens
+# Lemma ranking based on the token to lemma map
+python compute_stdev_by_lemma.py
 
 ######################
 # Crowdsourcing data building
@@ -24,23 +37,28 @@ grep -wf top-50-sorted-lemmas true-positives | cut -f 1 | perl -ne 'print lc' | 
 # very rough grep on selected lexical units (infinite tense only)
 for lu in {nascere,dare,uccidere,dire,morire}; do grep -rhw $lu ../../corpus/ | sed "s/$lu/<strong>$lu<\/strong>/gi" > $lu/tagged; done
 # get the token list from a verb
-grep -P '^(mor|muo)' ../../verbs/top-50-sorted-tokens
+grep -P '^(mor|muo)' ../../verbs/top-50-sorted-tokens > tokens
 # better grep using a token list (all tense occurrencies)
 grep -rhwf tokens ../../../corpus/ > raw-sentences
-# surround lexical units with a <strong> tag
-sed -r 's/(mor[^ ]+|muo[^ ]+)/<strong>\1<\/strong>/gi' raw-sentences > tagged-sentences
 # get sentences with less than 15 words
-while read f; do words=$(echo $f | wc -w); if [ $words -lt 15 ]; then echo $f >> short; fi; done < tagged-sentences
+while read f; do words=$(echo $f | wc -w); if [ $words -lt 15 ]; then echo $f >> short; fi; done < raw-sentences
+### DEPRECATED
 # gold creation: 10 random sentences for each lexical unit
-for lu in {nascere,dare,uccidere,dire,morire}; do shuf -n 10 $lu/tagged > $lu/gold; cat $lu/gold >> gold; done
+#for lu in {nascere,dare,uccidere,dire,morire}; do shuf -n 10 $lu/tagged > $lu/gold; cat $lu/gold >> gold; done
 # split into sentences
-python split_sentence.py
+#python split_sentence.py
+###
+### Gold creation
+# POS tag short sentences
+i=0; while read f; do echo $f | treetagger/cmd/tree-tagger-italian > training/itwiki/pos-tagged-gold/$i; let i++; done < training/itwiki/short
+# Filter SN V SN sentences
+python get_meaningful_sentences soccer/training/giocare/pos-tagged soccer/training/giocare/tokens
 # link entities
 python entity_linking.py ../../training/itwiki/clean-gold && mkdir ../../training/itwiki/linked-gold && mv *.json ../../training/itwiki/linked-gold
-# POS tag sentences
-i=0; while read f; do echo $f | treetagger/cmd/tree-tagger-italian > training/itwiki/pos-tagged-gold/$i; let i++; done < training/itwiki/clean-gold
+# surround lus with <strong> tag
+sed -r 's/(mor[^ ]+|muo[^ ]+)/<strong>\1<\/strong>/gi' raw-sentences > tagged-sentences
 # build crowdflower input csv
-python create_crowdflower_input.py ../training/itwiki/partecipare/linked-gold/ ../training/itwiki/partecipare/pos-tagged-gold/
+python create_crowdflower_input.py ../training/itwiki/partecipare/pos-tagged-gold/ ../training/itwiki/partecipare/linked-gold/
 
 #######################
 # Intersect with Evalita 2008 frame annotation dataset and get what matches
