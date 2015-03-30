9e39a8507cc7f91045525f58cb1f817b4727a4dd
Marco Fossati <fossati@fbk.eu>
2015-03-30 16:38:06 +0200

    renamed input spreadsheet fields

--- generate_crowdflower_interface_template.py
+++ generate_crowdflower_interface_template.py
@@ -19,9 +19,9 @@ FOOTER = "</div>"
 # token block template
 TOKEN_TEMPLATE = '''
     <!-- BEGIN token %(question_num)d -->
-    {%% if %(fe_field)s != 'No data available' %%}
+    {%% if %(token_field)s != 'No data available' %%}
     <div class="span2">
-        <cml:radios label="{{%(fe_field)s}}" class="rando" name="{{%(fe_field)s}}" validates="required" gold="true">
+        <cml:radios label="{{%(token_field)s}}" class="rando" name="{{%(token_field)s}}" validates="required" gold="true">
             <cml:radio label="Nessuno"></cml:radio>
             %(fe_blocks)s
         </cml:radios>
@@ -32,8 +32,8 @@ TOKEN_TEMPLATE = '''
 
 # fe block template
 FE_TEMPLATE = '''
-          {%% if %(fe_name_field)s != 'No data available' %%}
-          <cml:radio label="{{%(fe_name_field)s}}"></cml:radio>
+          {%% if %(fe_field)s != 'No data available' %%}
+          <cml:radio label="{{%(fe_field)s}}"></cml:radio>
           {%% endif %%}'''
 
 
@@ -43,21 +43,22 @@ def generate_crowdflower_interface_template(input_csv, output_html=None):
     sheet = csv.DictReader(input_csv)
     fields = sheet.fieldnames
     # Get "fe_name[0-9]" fields
-    fe_name_fields = [f for f in fields if re.match('fe_name[0-9]{2}$', f)]
-    # Get "fe[0-9]" fields
     fe_fields = [f for f in fields if re.match('fe[0-9]{2}$', f)]
+    # Get "fe[0-9]" fields
+    token_fields = [f for f in fields if re.match('token[0-9]{2}$', f)]
     # Generate fe blocks for every fe filed
     fe_blocks = []
-    for fe_name_field in fe_name_fields:
-        fe_blocks.append(FE_TEMPLATE % {'fe_name_field': fe_name_field})
+    for fe_field in fe_fields:
+        fe_blocks.append(FE_TEMPLATE % {'fe_field': fe_field})
     crowdflower_interface_template = HEADER
     # Generate fe_name blocks(question blocks) for every fe_name field
-    for idx, fe_field in enumerate(fe_fields):
-        dic = {'question_num': idx+1, 'fe_field': fe_field}
+    for idx, token_field in enumerate(token_fields):
+        dic = {'question_num': idx+1, 'token_field': token_field}
         # Add fe blocks into template
         dic['fe_blocks'] = ''.join(fe_blocks)
         # Add current fe_name block or question block into template
         crowdflower_interface_template += (TOKEN_TEMPLATE % dic)
+
     crowdflower_interface_template += FOOTER
     if output_html is None:
         return crowdflower_interface_template
