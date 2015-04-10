0fc0d4f486c8a389f80102b21cea605838a80f32
Marco Fossati <fossati@spaziodati.eu>
2015-04-10 11:28:25 +0200

    combine all chunks

--- combine_chunks.py
+++ combine_chunks.py
@@ -4,16 +4,19 @@
 import codecs
 import json
 import os
+import re
 import sys
+from collections import defaultdict
 
 debug = True
-# all_links = all_ngrams = all_tp = {}
 all_chunks = {}
 
 # Loop over the dirs containing the chunks
 for path, subdirs, files in os.walk(sys.argv[1]):
     for name in files:
         f = os.path.join(path, name)
+        # Skip unwanted files
+        if not re.match('^[0-9]', name): continue 
         # Standard sentence ID
         sentence_id = '%02d' % int(os.path.splitext(name)[0])
         # links
@@ -37,11 +40,11 @@ for path, subdirs, files in os.walk(sys.argv[1]):
             if debug:
                 print 'NGRAMS'
                 print ngram_chunks
-            all_chunks[sentence_id] = {'twm-ngrams': ngram_chunks}
+            all_chunks[sentence_id]['twm-ngrams'] = ngram_chunks
         # TextPro
         elif 'textpro-chunks' in path:
-            with codecs.open(f, 'rb', 'utf-8'):
-                tp = [l.strip() for l in f.readlines()]
+            with codecs.open(f, 'rb', 'utf-8') as i:
+                tp = [l.strip() for l in i.readlines()]
             tmp_tp_chunks = []
             # Parse TextPro format
             for line in tp:
@@ -58,47 +61,56 @@ for path, subdirs, files in os.walk(sys.argv[1]):
             for chunk in tmp_tp_chunks:
                 if chunk not in tp_chunks: tp_chunks.append(chunk)
             tp_chunks = set([' '.join(chunk) for chunk in tp_chunks])
-            all_chunks[sentence_id] = {'textpro-chunks': tp_chunks}
+            all_chunks[sentence_id]['textpro-chunks'] = tp_chunks
 
 if debug:
     print all_chunks
 
+combined = {}
 
-# If chunks overlap, prefer links > ngrams > chunker
-to_remove = set()
-for link_chunk in link_chunks:
+# Combine results
+for sentence, chunks in all_chunks.iteritems():
+    # If chunks overlap, prefer links > ngrams > chunker
+    link_chunks = chunks['twm-links']
+    ngram_chunks = chunks['twm-ngrams']
+    tp_chunks = chunks['textpro-chunks']
+    # Prune ngrams from links
+    to_remove = set()
+    for link_chunk in link_chunks:
+        for ngram_chunk in ngram_chunks:
+            if link_chunk in ngram_chunk:
+                to_remove.add(ngram_chunk)
+    ngram_chunks.difference_update(to_remove)
+
+    print 'NGRAMS PRUNED FROM LINKS'
+    print ngram_chunks
+
+    print 'TEXTPRO'
+    print tp_chunks
+    # Prune TextPro chunks from links
+    to_remove = set()
+    for link_chunk in link_chunks:
+        for tp_chunk in tp_chunks:
+            if link_chunk in tp_chunk:
+                to_remove.add(tp_chunk)
+    tp_chunks.difference_update(to_remove)
+
+    print 'TEXTPRO PRUNED FROM LINKS'
+    print tp_chunks
+
+    # Prune TextPro chunks from ngrams
+    to_remove = set()
     for ngram_chunk in ngram_chunks:
-        if link_chunk in ngram_chunk:
-            to_remove.add(ngram_chunk)
-ngram_chunks.difference_update(to_remove)
+        for tp_chunk in tp_chunks:
+            if ngram_chunk in tp_chunk:
+                to_remove.add(tp_chunk)
+    tp_chunks.difference_update(to_remove)
 
-print 'NGRAMS PRUNED FROM LINKS'
-print ngram_chunks
+    print 'TEXTPRO PRUNED FROM NGRAMS'
+    print tp_chunks
 
-print 'TEXTPRO'
-print tp_chunks
+    combined[sentence_id] = tp_chunks.union(ngram_chunks, link_chunks)
 
-to_remove = set()
-for link_chunk in link_chunks:
-    for tp_chunk in tp_chunks:
-        if link_chunk in tp_chunk:
-            to_remove.add(tp_chunk)
-tp_chunks.difference_update(to_remove)
-
-print 'TEXTPRO PRUNED FROM LINKS'
-print tp_chunks
-
-to_remove = set()
-for ngram_chunk in ngram_chunks:
-    for tp_chunk in tp_chunks:
-        if ngram_chunk in tp_chunk:
-            to_remove.add(tp_chunk)
-tp_chunks.difference_update(to_remove)
-
-print 'TEXTPRO PRUNED FROM NGRAMS'
-print tp_chunks
-
-all_chunks = tp_chunks.union(ngram_chunks, link_chunks)
 
 print 'FINAL'
-print all_chunks
+print combined
