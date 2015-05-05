05cb2393a7afcde8e390171a9d92ff148628eb1a
emilio <emilio.dorigatti@gmail.com>
2015-05-05 20:09:19 +0200

    initial date normalization module
    fixed grammar file to so that the generated python files are
    syntactically valid

---
+++ date_normalizer/DateAndTimeListener.py
@@ -0,0 +1,332 @@
+# Generated from java-escape by ANTLR 4.5
+from antlr4 import *
+
+
+
+# This class defines a complete listener for a parse tree produced by DateAndTimeParser.
+class DateAndTimeListener(ParseTreeListener):
+
+    # Enter a parse tree produced by DateAndTimeParser#value.
+    def enterValue(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#value.
+    def exitValue(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#date_or_time.
+    def enterDate_or_time(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#date_or_time.
+    def exitDate_or_time(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#day_duration.
+    def enterDay_duration(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#day_duration.
+    def exitDay_duration(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#week_duration.
+    def enterWeek_duration(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#week_duration.
+    def exitWeek_duration(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#month_duration.
+    def enterMonth_duration(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#month_duration.
+    def exitMonth_duration(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#year_duration.
+    def enterYear_duration(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#year_duration.
+    def exitYear_duration(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#day_relative.
+    def enterDay_relative(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#day_relative.
+    def exitDay_relative(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#day_absolute.
+    def enterDay_absolute(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#day_absolute.
+    def exitDay_absolute(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#time_relative.
+    def enterTime_relative(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#time_relative.
+    def exitTime_relative(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#month_relative.
+    def enterMonth_relative(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#month_relative.
+    def exitMonth_relative(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#year_relative.
+    def enterYear_relative(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#year_relative.
+    def exitYear_relative(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#week_number.
+    def enterWeek_number(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#week_number.
+    def exitWeek_number(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#week_relative.
+    def enterWeek_relative(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#week_relative.
+    def exitWeek_relative(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#year.
+    def enterYear(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#year.
+    def exitYear(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#date_repeat.
+    def enterDate_repeat(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#date_repeat.
+    def exitDate_repeat(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#time_start.
+    def enterTime_start(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#time_start.
+    def exitTime_start(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#time_number.
+    def enterTime_number(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#time_number.
+    def exitTime_number(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#ampm.
+    def enterAmpm(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#ampm.
+    def exitAmpm(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#date.
+    def enterDate(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#date.
+    def exitDate(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#maybesettimana.
+    def enterMaybesettimana(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#maybesettimana.
+    def exitMaybesettimana(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#maybedi.
+    def enterMaybedi(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#maybedi.
+    def exitMaybedi(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#maybethe.
+    def enterMaybethe(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#maybethe.
+    def exitMaybethe(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#eand.
+    def enterEand(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#eand.
+    def exitEand(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#maybealle.
+    def enterMaybealle(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#maybealle.
+    def exitMaybealle(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#maybeore.
+    def enterMaybeore(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#maybeore.
+    def exitMaybeore(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#month.
+    def enterMonth(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#month.
+    def exitMonth(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#duration.
+    def enterDuration(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#duration.
+    def exitDuration(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#timecomponents.
+    def enterTimecomponents(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#timecomponents.
+    def exitTimecomponents(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#rel_time_start.
+    def enterRel_time_start(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#rel_time_start.
+    def exitRel_time_start(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#season.
+    def enterSeason(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#season.
+    def exitSeason(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#holiday.
+    def enterHoliday(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#holiday.
+    def exitHoliday(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#season_relative.
+    def enterSeason_relative(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#season_relative.
+    def exitSeason_relative(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#number.
+    def enterNumber(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#number.
+    def exitNumber(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#threedignum.
+    def enterThreedignum(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#threedignum.
+    def exitThreedignum(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#yearnum.
+    def enterYearnum(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#yearnum.
+    def exitYearnum(self, ctx):
+        pass
+
+
+    # Enter a parse tree produced by DateAndTimeParser#timenum.
+    def enterTimenum(self, ctx):
+        pass
+
+    # Exit a parse tree produced by DateAndTimeParser#timenum.
+    def exitTimenum(self, ctx):
+        pass
+
+
---
+++ date_normalizer/build-grammar.sh
@@ -0,0 +1,2 @@
+java -jar ../../lib/antlr-4.5-complete.jar DateAndTime.g4 -o . -Dlanguage=Python2
+#sed -i 's/import java.util.*;//p' *.py
