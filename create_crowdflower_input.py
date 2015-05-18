08364cdc6ddbf1be518707a4709df09bdca3539b
Marco Fossati <fossati@fbk.eu>
2015-05-18 14:57:01 +0200

    handle disambiguated (AKA known) FEs and fill in the remaining ones from the frame definitions

--- create_crowdflower_input.py
+++ create_crowdflower_input.py
@@ -11,63 +11,58 @@ import sys
 from collections import defaultdict
 
 
-LU_FRAME_MAP_LOCATION = 'resources/soccer-lu2frame.json'
+LU_FRAME_MAP_LOCATION = 'resources/soccer-lu2frame-dbptypes.json'
 LU_FRAME_MAP = json.load(open(LU_FRAME_MAP_LOCATION))
-TOKENS = []
 
 
-def load_all_tokens():
-    for lu in LU_FRAME_MAP:
-        for token in lu['lu']['tokens']:
-            TOKENS.append(token)
-
-
-def prepare_crowdflower_input(chunk_data, debug):
+def prepare_crowdflower_input(labeled_data, chunk_data, debug):
     input_data = []
-    for sentence in chunk_data:
+    # Update labeled data with chunks
+    for labeled in labeled_data:
+        for chunk in chunk_data:
+            if chunk['id'] == labeled['id']:
+                labeled.update(chunk)
+    for sentence in labeled_data:
         if debug:
-            print 'CHUNK DATA: %s' % sentence
+            print 'LABELED DATA: %s' % sentence
+        chunks = sentence['chunks']
         input_row = {}
         input_row['id'] = sentence['id']
         snt = sentence['sentence']
         input_row['sentence'] = snt
-        # Tokenize by splitting on spaces
-        sentence_tokens = snt.split()
+        input_row['lu'] = sentence['lu']
+        input_row['frame'] = sentence['frame']
         if debug:
             print 'ID: %s' % input_row['id']
             print 'SENTENCE: %s' % input_row['sentence']
-            print 'TOKENS: %s' % sentence_tokens
-        frames = []
-        for lu in LU_FRAME_MAP:
-            lu_tokens = lu['lu']['tokens']
-            # Check if a sentence token matches a LU token and assign frames accordingly
-            for sentence_token in sentence_tokens:
-                if sentence_token in lu_tokens:
-                    if debug:
-                        print 'SENTENCE TOKEN "%s" MATCHED IN LU TOKENS' % sentence_token
-                    input_row['lu'] = lu['lu']['lemma']
-                    frames = lu['lu']['frames']
-                    if debug:
-                        print 'LU LEMMA: %s' % input_row['lu']
-                        print 'FRAMES: %s' % frames
-                    for frame in frames:
-                        # TODO this will overwrite in case of more frames per LU
-                        input_row['frame'] = frame['frame']
-                        fe_names = frame['FEs']
-                        if debug:
-                            print 'ASSIGNED FRAME: %s' % frame
-                            print 'FEs: %s' % fe_names
-                        # Store FE and chunks with incremental numbers
-                        # fe_name{i}, fe{j}
-                        for i in xrange(0, len(fe_names)):
-                            # Also store FE type (core or extra)
-                            input_row['fe_name%02d' % i], input_row['fe_name%02d_type' % i] = fe_names[i].items()[0]
-                            if debug:
-                                print 'FIELD fe_name%02d: %s' % (i, fe_names[i])
-                        for j in xrange(0, len(sentence['chunks'])):
-                            input_row['fe%02d' % j] = sentence['chunks'][j]
-                            if debug:
-                                print 'FIELD fe%02d: %s' % (j, sentence['chunks'][j])
+            print 'ALL CHUNKS: %s' % chunks
+        fe_names = [fe['FE'] for fe in sentence['FEs']]
+        for i, fe in enumerate(sentence['FEs']):
+            chunk = fe['chunk']
+            input_row['known_fe%02d' % i] = chunk
+            input_row['known_fe_name%02d' % i] = fe['FE']
+            input_row['known_fe_name_type%02d' % i] = fe['type']
+            # Remove already labeled chunks
+            if chunk in chunks:
+                chunks.remove(chunk)
+        if debug:
+            print 'PRUNED CHUNKS: %s' % chunks
+        # Add FEs that were not retrieved by the unsupervised strategy
+        for lu_obj in LU_FRAME_MAP:
+            lu = lu_obj['lu']['lemma']
+            if not lu == input_row['lu']:
+                continue
+            for frame in lu_obj['lu']['frames']:
+                if not frame['frame'] == input_row['frame']:
+                    continue
+                for j, fe in enumerate(frame['FEs']):
+                    fe_name, fe_type = fe.items()[0] # Always only one dict
+                    if fe_name in fe_names:
+                        continue
+                    input_row['fe_name%02d' % j] = fe_name
+                    input_row['fe_name%02d_type' % j] = fe_type
+                for k, chunk in enumerate(chunks):
+                    input_row['fe%02d' % k] = chunk
         if debug:
             print 'COMPLETE ROW: %s' % input_row
         # Prepare input for DictWriter, since it won't write UTF-8
@@ -83,7 +78,7 @@ def write_input_spreadsheet(input_data, outfile, debug):
     gold_columns = []
     for field in fields:
         # Add gold answer columns for each token
-        if re.match('fe[0-9]{2}$', field): gold_columns.append(field + '_gold')
+        if re.search('fe[0-9]{2}$', field): gold_columns.append(field + '_gold')
     fields += gold_columns
     fields.sort()
     if debug:
@@ -96,7 +91,8 @@ def write_input_spreadsheet(input_data, outfile, debug):
 
 def create_cli_parser():
     parser = argparse.ArgumentParser(description='Build input CSV for a Semantic Role Labeling CrowdFlower job')
-    parser.add_argument('chunk_data', help='JSON file containing chunk data for each sentence')
+    parser.add_argument('labeled_data', help='JSON file containing labeled data for each sentence')
+    parser.add_argument('chunks', help='JSON file containing chunks for each sentence')
     parser.add_argument('-o', '--output', default='crowdflower_input.csv', type=argparse.FileType('wb'), help='Write output to the given file')
     parser.add_argument('--debug', action='store_true', help='Toggle debug mode')
     return parser
@@ -105,7 +101,8 @@ def create_cli_parser():
 if __name__ == "__main__":
     cli = create_cli_parser()
     args = cli.parse_args()
-    chunk_data = json.load(codecs.open(args.chunk_data, 'rb', 'utf-8'))
-    input_data = prepare_crowdflower_input(chunk_data, args.debug)
+    labeled_data = json.load(codecs.open(args.labeled_data, 'rb', 'utf-8'))
+    chunks = json.load(codecs.open(args.chunks, 'rb', 'utf-8'))
+    input_data = prepare_crowdflower_input(labeled_data, chunks, args.debug)
     write_input_spreadsheet(input_data, args.output, args.debug)
     sys.exit(0)
