2eb9e6437b912f31bab597924eb4ef125097f7bc
Marco Fossati <fossati@spaziodati.eu>
2015-08-31 16:44:12 +0200

    move the scores dataset output file to the 3rd positional arg

--- unsupervised/labeled_to_assertions.py
+++ unsupervised/labeled_to_assertions.py
@@ -14,16 +14,16 @@ from lib.to_assertions import to_assertions
 @click.command()
 @click.argument('labeled')
 @click.argument('wid-title-mapping', type=click.File('r', 'utf8'))
+@click.argument('scores', default='scores.nt', type=click.File('w'))
 @click.argument('processed_out', default='processed')
 @click.argument('discarded_out', default='discarded')
 @click.argument('dataset', default='dataset.nt')
-@click.argument('scores', default='scores.nt', type=click.File('w'))
 @click.option('--format', default='nt')
 @click.option('--resource-namespace', default='http://it.dbpedia.org/resource/')
 @click.option('--fact-namespace', default='http://dbpedia.org/fact-extraction/')
 @click.option('--ontology-namespace', default='http://dbpedia.org/ontology/')
-def main(labeled, wid_title_mapping, processed_out, discarded_out, dataset, format,
-         resource_namespace, fact_namespace, ontology_namespace, scores):
+def main(labeled, wid_title_mapping, scores, processed_out, discarded_out, dataset, format,
+         resource_namespace, fact_namespace, ontology_namespace):
 
     mapping = json.load(wid_title_mapping)
     with codecs.open(labeled, 'rb', 'utf8') as f:
